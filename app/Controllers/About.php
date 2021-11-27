500db70146bd3e1cc9b5f08c2570b4af8ad4de38
codehafeez <codehafeez@gmail.com>
2021-11-27 12:03:22 +0300

    codeigniter4 Example Code by Abdul Hafeez

---
+++ app/Controllers/About.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Controllers;
+class About extends BaseController {
+
+    public function index(){
+        echo "hello world";
+    }
+    
+}
---
+++ app/Views/welcome_message.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Welcome to CodeIgniter 4</title>
+</head>
+<body>
+
+<h1>Codeigniter4 - Home Page</h1>
+
+</body>
+</html>
